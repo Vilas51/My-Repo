8bfc7b8cb3f853e09e209e76c5503468e84445f7
Vilas51 <ganyabachhav@gmail.com>
2024-06-05 18:22:51 +0530

    insertionmsort added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void bubblesort(){
     //
 }
+
+void quicksort(){
+    //
+}
+
+void insertionsort(){
+    //
+}
+
+//Inky Pinky Ponky
+
 int main(){
     cout<<"Hello World"<<endl;
     cout<<"Hehe "<<endl;
